e55bba43928fa4c3b740be15c98068c81c5db6e0
Gino Rey <gino.rey@outlook.com>
2022-08-03 13:22:06 -0400

    letter change

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd ~/itstimeapi
+cd ~/itstimeAPI
 git fetch && git reset origin/main --hard
 docker compose -f docker-compose.prod.yml down
 docker compose -f docker-compose.prod.yml up -d --build
